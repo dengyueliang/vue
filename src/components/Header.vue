e9c3286cd92a7ed557dab0ef20c9ce43cd15086e
HuangTao <2803253915@qq.com>
2018-11-28 11:22:49 +0800

    info界面取消全屏功能

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,12 +7,7 @@
         <div class="logo">爱宠邦后台管理系统</div>
         <div class="header-right">
             <div class="header-user-con">
-                <!-- 全屏显示 -->
-                <div class="btn-fullscreen" @click="handleFullScreen" >
-                    <el-tooltip effect="dark" :content="fullscreen?`取消全屏`:`全屏`">
-                        <i class="el-icon-rank"></i>
-                    </el-tooltip>
-                </div>
+             
                 <!-- 消息中心 -->
                 <div class="btn-bell">
                     <el-tooltip effect="dark"  :content="message?`有${message}条未读消息`:`消息中心`" placement="bottom">
@@ -56,33 +51,7 @@ export default {
       this.collapse = !this.collapse;
       bus.$emit("collapse", this.collapse);
     },
-    //   全屏事件
-    handleFullScreen() {
-      let element = document.documentElement;
-      if (this.fullscreen) {
-        if (document.exitFullscreen) {
-          document.exitFullscreen();
-        } else if (document.webkitCancelFullScreen) {
-          document.webkitCancelFullScreen();
-        } else if (document.mozCancelFullScreen) {
-          document.mozCancelFullScreen();
-        } else if (document.msExitFullscreen) {
-          document.msExitFullscreen();
-        }
-      } else {
-        if (element.requestFullscreen) {
-          element.requestFullscreen();
-        } else if (element.webkitRequestFullScreen) {
-          element.webkitRequestFullScreen();
-        } else if (element.mozRequestFullScreen) {
-          element.mozRequestFullScreen();
-        } else if (element.msRequestFullscreen) {
-          // IE11
-          element.msRequestFullscreen();
-        }
-      }
-      this.fullscreen = !this.fullscreen;
-    }
+  
   }
 };
 </script>
